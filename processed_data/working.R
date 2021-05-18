78bcc5e224139ee47c8e34d31d180248549971a9
DaniilPanshin <dany.panshin@yandex.ru>
2021-05-18 18:40:52 +0300

    5

--- processed_data/working.R
+++ processed_data/working.R
@@ -1,11 +1,12 @@
 raw_glioma <- read_xlsx('/Users/Donel/Desktop/Glioma/raw_data/Gliome.xlsx')
 raw_glioma <- raw_glioma[,1:57]
+work_gl <- select(raw_glioma, -Histology)
 
 str(raw_glioma)
 
 library(tidyverse)
 library(readxl)
-
+library(dplyr)
 
 
 names(raw_glioma)[names(raw_glioma) == "Гистология"] <- "Histology"
@@ -47,14 +48,14 @@ names(raw_glioma)[names(raw_glioma) == "ПОЛ (1-ж, 2-м)"] <- "Sex"
 names(raw_glioma)[names(raw_glioma) == "Послеоперационный обьем"] <- "Postoperative_volume"
 names(raw_glioma)[names(raw_glioma) == "Послеоперационный Обьем образования посчитанный при помощи сегментации"] <- "Postoperative_volume_segmentation"
 names(raw_glioma)[names(raw_glioma) == "Прогностический индекс"] <- "S_predictive_index"
-names(raw_glioma)[names(raw_glioma) == "размер обьеного образования >5см"] <- "C_size>5cm"
+names(raw_glioma)[names(raw_glioma) == "размер обьеного образования >5см"] <- "C_size_more"
 names(raw_glioma)[names(raw_glioma) == "Разница"] <- "Difference"
 names(raw_glioma)[names(raw_glioma) == "Речевые нарушения  (0- нет, 2- ненарушена способность общения, 3- нарушена способность общения, 4 - неспособность к общению)"] <- "Speech_disorders"
 names(raw_glioma)[names(raw_glioma) == "Сложность тотальной резекции по Giannantonio Spena 0-лег 1-ср 2-тяж"] <- "S_Difficulty_by_Spena"
 names(raw_glioma)[names(raw_glioma) == "Сложность тотальной резекции по Hani J. Marcus 0-лег 1-ср 2-тяж"] <- "M_difficulty_by_Marcus"
 names(raw_glioma)[names(raw_glioma) == "Сложность тотальной резекции по Jaume Capellades (0-лег 1-ср 2-тяж)"] <- "C_difficulty_by_Capellades"
 names(raw_glioma)[names(raw_glioma) == "Смещение срединных структур, мм"] <- "Displacement_midline_str_mm"
-names(raw_glioma)[names(raw_glioma) == "степень накопления КВ (0-нет, 1-да)"] <- "S_accumulation _CV"
+names(raw_glioma)[names(raw_glioma) == "степень накопления КВ (0-нет, 1-да)"] <- "S_accumulation_CV"
 names(raw_glioma)[names(raw_glioma) == "Сторона поражения (правое-1, левое-2, обе-3, мозолистое тело - 4)"] <- "The_affected_side"
 names(raw_glioma)[names(raw_glioma) == "Судороги (0 - нет, 2- фокальные, 3 - генерализованные, 4 - эпистатус)"] <- "Convulsions"
 names(raw_glioma)[names(raw_glioma) == "судорожный синдром (0-нет, 1-да)"] <- "S_convulsive_syndrome"
@@ -117,15 +118,14 @@ raw_glioma$[raw_glioma$ == '-'] <- '0'
 
 
 
-
-raw_glioma <- data %>% 
-  mutate(S_convulsive_syndrome = replace, S_convulsive_syndrome, S_convulsive_syndrome == 0, 'no')
+#Sex
+work_gl$Sex <- as.character(gsub(2,"man" , gsub(1,"woman" , work_gl$Sex)))
 
 work_gl$The_affected_side <- as.character(gsub(2,"Right" , gsub(1,"Left" ,gsub(3, "both_sides", gsub(4, "corpus_callosum", work_gl$The_affected_side)))))
-work_gl$The_affected_side <- as.character(gsub(0,"False" , gsub(1,"True" , work_gl$The_affected_side)))
+ 
 work_gl$S_convulsive_syndrome <- as.character(gsub(0,"False" , gsub(1,"True" , work_gl$S_convulsive_syndrome)))
 
-work_gl$S_convulsive_syndrome <- as.character(gsub(0,"False" , gsub(1,"True" , work_gl$S_convulsive_syndrome)))
+
 #
 work_gl$The_affected_side <- as.character(gsub(2,"Right" , gsub(1,"Left" ,gsub(3, "both_sides", gsub(4, "corpus_callosum", work_gl$The_affected_side)))))
 #
@@ -164,8 +164,64 @@ work_gl$S_clear_tumor_edges <- as.character(gsub(0,"False" , gsub(1,"True" , wor
 #S_dist_more_80
 work_gl$S_dist_more_80 <- as.character(gsub(0,"False" , gsub(1,"True" , work_gl$S_dist_more_80)))
 
+#S_convulsive_syndrome
+work_gl$S_convulsive_syndrome <- as.character(gsub(0,"False" , gsub(1,"True" , work_gl$S_convulsive_syndrome)))
 
+#S_neuro_deficit
+work_gl$S_neuro_deficit <- as.character(gsub(0,"False" , gsub(1,"True" , work_gl$S_neuro_deficit)))
 
+#S_MRI_index
+work_gl$S_MRI_index <- as.character(gsub(0,"False" , gsub(1,"True" , work_gl$S_MRI_index)))
+
+#S_accumulation_CV
+work_gl$S_accumulation_CV <- as.character(gsub(0,"False" , gsub(1,"True" , work_gl$S_accumulation_CV)))
+
+#S_Difficulty_by_Spena
+work_gl$S_Difficulty_by_Spena <- as.character(gsub(0,"easy" , gsub(1,"medium" , gsub(2,"hard" ,work_gl$S_Difficulty_by_Spena))))
+
+#M_less_10_distance
+work_gl$M_less_10_distance <- as.character(gsub(0,"False" , gsub(1,"True" , work_gl$M_less_10_distance)))
+
+#M_bilateral_location_corcall
+work_gl$M_bilateral_location_corcall <- as.character(gsub(0,"False" , gsub(1,"True" , work_gl$M_bilateral_location_corcall)))
+
+#M_funct_important_area
+work_gl$M_funct_important_area <- as.character(gsub(0,"False" , gsub(1,"True" , work_gl$M_funct_important_area)))
+
+#M_swelling
+work_gl$M_swelling <- as.character(gsub(0,"False" , gsub(1,"True" , work_gl$M_swelling)))
+
+#M_dist_more_40
+work_gl$M_dist_more_40 <- as.character(gsub(0,"False" , gsub(1,"True" , work_gl$M_dist_more_40)))
+
+#M_difficulty_by_Marcus
+work_gl$M_difficulty_by_Marcus <- as.character(gsub(0,"easy" , gsub(1,"medium" , gsub(2,"hard" ,work_gl$M_difficulty_by_Marcus))))
+
+#C_size>5cm
+work_gl$C_size_more <- as.character(gsub(0,"False" , gsub(1,"True" , work_gl$C_size_more)))
+
+#C_Functionally__structure
+work_gl$C_Functionally__structure <- as.character(gsub(0,"False" , gsub(1,"True" , work_gl$C_Functionally__structure)))
+
+#C_difficulty_by_Capellades
+work_gl$C_difficulty_by_Capellades <- as.character(gsub(0,"easy" , gsub(1,"medium" , gsub(2,"hard" ,work_gl$C_difficulty_by_Capellades))))
+
+#Movement_disorders
+work_gl$Movement_disorders <- as.character(gsub(2,"mild_hemimonoparesis" , gsub(1,"asymptomatic" ,
+                                                            gsub(3, "moderate_hemimonoparesis", 
+                                                                 gsub(4, "deep_hemiparesis_hemiplegia",
+                                                                    gsub(0, "false", work_gl$Movement_disorders))))))
+
+str(work_gl)
 
 raw_glioma$S_convulsive_syndrome
+
+#EDA
+
+library(ggplot2)
+library(PerformanceAnalytics)
+
+Metric_df <- select(work_gl, c(Largest_diameter, Great_perpendicular, Education_volume_mm, Volume_segmentation))
+
+chart.Boxplot(Metric_df)
 
